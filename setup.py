628622df1803893ec3a64c391ac9f7942a59e7b1
Thomas Watson <twatson52@icloud.com>
2020-04-11 14:13:45 -0500

    initial commit, set up really really basic package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tastaf",
+    version="0.1",
+    author="tpw_rules",
+    description="TASHA and friends; tools for console TAS replay",
+    packages=find_packages(),
+)
