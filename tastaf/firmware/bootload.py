2f2b22f2c920db89663d3ea470c5127e241b63cc
Thomas Watson <twatson52@icloud.com>
2020-04-21 21:51:28 -0500

    add basic PC bootload stuff with more absolutely no testing

---
+++ tastaf/firmware/bootload.py
@@ -0,0 +1,134 @@
+# bootload a system running the bootloader firmware
+
+import serial
+import time
+import struct
+
+import crcmod.predefined
+crc_16_kermit = crcmod.predefined.mkPredefinedCrcFun("kermit")
+
+from .bootloader_fw import ROM_INFO_WORDS, PACKET_MAX_LENGTH, BOOTLOADER_VERSION
+
+class BootloadError(Exception): pass
+
+class BadCRC(BootloadError):
+    def __init__(self, expected, received):
+        self.expected = expected
+        self.received = received
+
+    def __repr__(self):
+        return "BadCRC(expected=0x{:4X}, received=0x{:4X})".format(
+            self.expected, self.received)
+
+    def __str__(self):
+        return "Bad CRC: expected 0x{:04X} but received 0x{:04X}".format(
+            self.expected, self.received)
+
+class Timeout(BootloadError): pass
+
+class Bootloader:
+    def __init__(self):
+        # we don't have a port until we are connected to the target
+        self.port = None
+
+    def _ser_read(self, length):
+        read = b""
+        while length > 0:
+            new = ser.read(length)
+            if len(new) == 0:
+                raise Timeout("read timeout")
+            read += new
+            length -= len(new)
+        print(read)
+        return read
+
+    def _command(self, command, params):
+        if self.port is None:
+            raise BootloadError("not connected to target")
+
+        cmd_words = []
+        # start with the command word
+        cmd_words.append((command << 8) + len(params))
+        # then send all the parameters
+        cmd_words.extend(params)
+        cmd_bytes = struct.pack("{}<H".format(len(cmd_words)), *cmd_words)
+        # and finally, the CRC of everything above
+        cmd_bytes = cmd_bytes + \
+            crc_16_kermit(cmd_bytes).to_bytes(2, byteorder="little")
+
+        # reset the serial transmission to avoid sending or receiving junk from
+        # previous (potentially errored) commands
+        self.port.reset_input_buffer()
+        self.port.reset_output_buffer()
+        # then send the current command
+        self.port.write(cmd_bytes)
+
+        # read back the response word (which is secretly two bytes)
+        resp_length = self._ser_read(1)[0]
+        resp_code = self._ser_read(1)[0]
+        resp_bytes = ser_read(ser, 2*(resp_length+1)) # + 1 to account for CRC
+        resp_data = struct.unpack("{}<H".format(resp_length), resp_bytes)
+        
+        # verify CRC (not including the CRC itself)
+        calc_crc = crc_16_kermit(resp_bytes[:-2])
+        resp_data, claimed_crc = resp_data[:-1], resp_data[-1]
+        if calc_crc != claimed_crc:
+            raise BadCRC(crc, claimed_crc)
+
+        # handle error response
+        if resp_code == 2:
+            problems = {0: "unknown command", 1: "invalid length",
+                2: "bad CRC", 3: "timeout"}
+            raise BLError("was told '{}'".format(
+                problems.get(resp_data[0], resp_data[0])))
+
+        return resp_code, resp_data
+
+    # connect to the target on serial port "port". give up after (about)
+    # "timeout" seconds. return if connected or throw exception if failure
+    def connect(self, port, timeout=None):
+        # create a serial port to connect to the target. we set a 600ms timeout,
+        # slightly over the 500ms timeout in the firmware, to make sure we
+        # receive timeout errors.
+
+        port = serial.Serial(port=port, baudrate=2_000_000, timeout=0.6)
+        self.port = port
+
+        try:
+            if timeout is not None:
+                timed_out = time.monotonic()+timeout
+            while timeout is None or (time.monotonic() < timed_out):
+                # say hello
+                try:
+                    resp_code, resp_data = self._command()
+                except Exception as e:
+                    # ignore any errors and try to do it again, hopefully when
+                    # the target has timed out and everything is reset.
+                    continue
+                # if we succeeded, we are done
+                if resp_code == 1:
+                    return # port is saved and we are connected
+                else:
+                    raise BootloaderError("unexpected response {}".format(
+                        resp_code))
+            else: # loop condition failed, i.e. we timed out
+                raise Timeout("connection timeout")
+        except:
+            self.port = None # we did not actually connect
+            raise
+
+def do_bootload(port, program):
+    print("Connecting...")
+
+    bootloader = Bootloader()
+
+    # try to connect while assuming the board is responsive
+    try:
+        bootloader.connect(port, timeout=2)
+    except Timeout: # it wasn't.
+        # ask user to try and reset it so the bootloader starts
+        print("    (board is unresponsive, please reset it)")
+        bootloader.connect(port, timeout=None)
+
+    print("Connected!")
+
---
+++ tastaf/firmware/bootload_test.py
@@ -0,0 +1,38 @@
+import sys
+
+from boneless.arch.opcode import Instr
+from boneless.arch.opcode import *
+
+from ..gateware.periph_map import p_map
+from .bootload import do_bootload
+
+# test program to confirm things are working
+fw = [
+L("rx_wait"),
+    LDXA(R1, p_map.uart.r_rx_lo),
+    ROLI(R1, R1, 1),
+    BS1("rx_wait"),
+
+    MOVR(R0, "hi"),
+
+L("tx_wait"),
+    LDXA(R1, p_map.uart.r_tx_status),
+    ANDI(R1, R1, 1),
+    BZ0("tx_wait"),
+
+    LD(R1, R0, 0),
+    CMPI(R1, 0),
+    BEQ("done"),
+    STXA(R1, p_map.uart.w_tx_lo),
+    ADDI(R0, R0, 1),
+    J("tx_wait"),
+
+L("done"),
+    J("done"),
+
+L("hi"),
+    list(ord(c) for c in "Hello, world!"),
+]
+
+# bootload the program to the board on the given port
+do_bootload(sys.argv[1], Instr.assemble(fw))
