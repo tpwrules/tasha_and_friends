fd988eeadc01dcbfb83f71159610006aabf306cf
Thomas Watson <twatson52@icloud.com>
2020-05-03 22:16:07 -0500

    properly parse play arguments and add option to add/remove latches

--- tasha/host/play.py
+++ tasha/host/play.py
@@ -1,13 +1,23 @@
-# play back a TAS
-
-import sys
+import argparse
 import time
 
 import numpy as np
 
 from .latch_streamer import LatchStreamer
 
-latch_file = open(sys.argv[2], "rb")
+parser = argparse.ArgumentParser(description='Play back a TAS using TASHA.')
+parser.add_argument('port', type=str,
+    help='Name of the serial port TASHA is attached to.')
+parser.add_argument('file', type=argparse.FileType('rb'),
+    help='Path to the r16m file to play back.')
+
+parser.add_argument('-b', '--blank', type=int, default=0,
+    help='Prepend blank latches to or (if negative) remove latches from the '
+    'start of the TAS.')
+
+args = parser.parse_args()
+
+latch_file = args.file
 def read_latches(num_latches):
     # for legacy reasons, the latch file holds data for 8 controllers, but
     # people only use at max 4. it's also big endian, unlike everything else.
@@ -23,8 +33,18 @@ def read_latches(num_latches):
     return out
 
 latch_streamer = LatchStreamer()
+
+if args.blank > 0:
+    latch_streamer.add_latches(np.zeros((args.blank, 5), dtype=np.uint16))
+elif args.blank < 0:
+    to_read = -args.blank
+    while to_read > 0:
+        l = read_latches(to_read)
+        to_read -= len(l)
+    l = None
+
 latch_streamer.add_latches(read_latches(100))
-latch_streamer.connect(sys.argv[1], status_cb=print)
+latch_streamer.connect(args.port, status_cb=print)
 
 while True:
     while latch_streamer.latch_queue_len < 10000:
