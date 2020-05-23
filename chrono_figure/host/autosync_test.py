f4fa2b0bd7a1b2fd76404d0d4ab81ba42785b6b4
Thomas Watson <twatson52@icloud.com>
2020-05-23 10:51:42 -0500

    chrono_figure.host.autosync_test: revamp algorithm to be coded less bad and also use more logical data structure

--- chrono_figure/host/autosync_test.py
+++ chrono_figure/host/autosync_test.py
@@ -4,6 +4,7 @@ import sys
 import time
 import json
 import random
+from collections import namedtuple
 
 import numpy as np
 
@@ -11,13 +12,26 @@ from .interface import ChronoFigureInterface
 from tasha.host.latch_streamer import LatchStreamer
 from tasha.gateware.apu_calc import calculate_advanced
 
-F_CYC = 357366 # number of clock cycles per frame
+# number of master clock cycles per frame
+F_CYC = 357366
+# frequency of master clock in MHz. currently only used during clock balancing
+# where i don't think it's value matters actually? it might?
+M_FREQ = 21.477272
+# number of APU register accesses needed to consider a group as accessing the
+# APU. many games (i.e. dkc2 and smw) access the APU 4 times per frame to play
+# sound effects, so we set it higher than that to avoid fiddling with those.
+MIN_APU_ACCESSES = 5
+# how close (in percent of frame time) an NMI can get to a frame boundary before
+# we try to fix it
+NMI_CLOSE = 10
+# how far away we aim to move a close NMI
+NMI_CLOSE_TARGET = 20
 
 if len(sys.argv) != 5:
     print("args: timeline_in timeline_out r16m tasha_port")
     exit(1)
 
-print("loading")
+print("loading...")
 
 timeline = json.load(open(sys.argv[1], "r"))
 t_nmis = timeline["nmis"]
@@ -30,61 +44,96 @@ def read_all_latches():
 
 all_latches = read_all_latches()
 
-# fill with default clock frequency
+# remove blank trailing latches
+nr = 0
+for r in all_latches[::-1]:
+    if np.any(r != 0):
+        break
+    nr += 1
+nr -= 600
+if nr > 0:
+    all_latches = all_latches[:-nr]
+
+# default (and on-powerup) clock frequency
 default_basic, default_advanced, default_real = \
-    calculate_advanced(24.607104, jitter=4)
-freqs = [default_real]*len(all_latches)
-all_latches[:, 4] = default_basic
-all_latches[:, 5] = default_advanced
+    calculate_advanced(24.607104)
 
-# figure out what latch each nmi starts at. this is when we will change the freq
-nmi_start_latches = []
-latch_num = 0
-last_nmi_start = 0
-t_ls = timeline["latches"]
-for nmi in t_nmis:
-    num_cycles = nmi["end_cycle"]-last_nmi_start
-    last_nmi_start = nmi["end_cycle"]
-    # we start at this latch
-    nmi_start_latches.append(latch_num)
-    # but how many should we go?
-    orig_latch_num = latch_num
-    while latch_num < len(t_ls) and t_ls[latch_num] < (nmi["end_cycle"] - 1000):
-        latch_num += 1
-    if orig_latch_num == latch_num:
-        raise Exception("oop!")
+# hold one group of NMIs that theoretically are related and whose APU frequency
+# will be controlled as one. one group consists of a set of contiguous NMIs with
+# the first zero or more being 100% busy and the last being not 100% busy. this
+# should comprise one frame of game logic.
+NMIGroup = namedtuple("NMIGroup", [
+    "nmi_start", # index of the starting NMI in this group
+    "num_nmis", # number of NMIs in the group
+    "latch_start", # latch for the first NMI in the group
+    "num_latches", # how many latches in the group
+    "apu_accesses", # how many times this group accesses the APU
+    "ex_end_cycle", # expected values from the emulator
+    "ex_wait_cycle",
+    "apu_freqs", # list of APU frequencies applied
+    "deviations", # deviations (measured_wait-ex_end_cycle) for each frequency
+])
 
-# validate that all nmis start close to their latch (or rather that they end
-# close to the next)
-for nmi, latch in zip(t_nmis[:-1], nmi_start_latches[:-1]):
-    p = nmi_start_latches.index(latch)
-    if abs(t_ls[nmi_start_latches[p+1]]-nmi["end_cycle"]) > 1000:
-        print(nmi, t_ls[nmi_start_latches[p+1]], nmi["end_cycle"])
+def group():
+    # figure out the latch each nmi starts at. it's when we will change the freq
+    nmi_start_latches = []
+    latch_num = 0
+    last_nmi_start = 0
+    t_ls = timeline["latches"]
+    for nmi in t_nmis:
+        num_cycles = nmi["end_cycle"]-last_nmi_start
+        last_nmi_start = nmi["end_cycle"]
+        # we start at this latch
+        nmi_start_latches.append(latch_num)
+        # but how many should we go?
+        orig_latch_num = latch_num
+        while latch_num<len(t_ls) and t_ls[latch_num]<(nmi["end_cycle"] - 1000):
+            latch_num += 1
+        if orig_latch_num == latch_num:
+            raise Exception("oop!")
+    nmi_start_latches.append(len(t_ls))
 
-# group nmis together so that the last one also controls the others
-group_map = {} # from nmi to the last one
-grouped_nmis = []
-for ni, nmi in enumerate(t_nmis):
-    # if this nmi doesn't access the apu, we don't care
-    if nmi["apu_reads"] + nmi["apu_writes"] < 4:
-        grouped_nmis = []
-        continue
-    ex_end_cycle, ex_wait_cycle = nmi["end_cycle"], nmi["wait_cycle"]
-    ex_busy = (F_CYC-ex_end_cycle+ex_wait_cycle)/F_CYC*100
-    # is this nmi 100% busy?
-    if ex_busy > 99.99:
-        # it might be a group member
-        grouped_nmis.append(ni)
-    else: # it's the controller
-        if len(grouped_nmis) == 0: continue
-        # assign the first one's latch to the controller
-        nmi_start_latches[ni] = nmi_start_latches[grouped_nmis[0]]
-        # map them all to the controller
-        for gni in grouped_nmis:
-            group_map[gni] = ni
-        print("gj:", t_nmis[grouped_nmis[0]]["joy_reads"],
-            ",", nmi["joy_writes"], ni)
-        grouped_nmis = []
+    # validate that all nmis start close to their latch (or rather that they end
+    # close to the next)
+    for nmi, latch in zip(t_nmis[:-1], nmi_start_latches[:-1]):
+        p = nmi_start_latches.index(latch)
+        if abs(t_ls[nmi_start_latches[p+1]]-nmi["end_cycle"]) > 1000:
+            print(nmi, t_ls[nmi_start_latches[p+1]], nmi["end_cycle"])
+
+    nmi_groups = []
+    these_nmis = []
+    for ni, nmi in enumerate(t_nmis):
+        # this nmi is a part of this group
+        these_nmis.append(ni)
+        if nmi["end_cycle"] == nmi["wait_cycle"]: # is this NMI 100% busy?
+            # yup, so there has to be another one in this group
+            continue
+
+        first_nmi = these_nmis[0]
+        last_nmi = these_nmis[-1]
+
+        apu_accesses = 0
+        for gnmi in t_nmis[first_nmi:last_nmi+1]:
+            apu_accesses += (gnmi["apu_reads"]+gnmi["apu_writes"])
+
+        this_group = NMIGroup(
+            nmi_start=first_nmi,
+            num_nmis=len(these_nmis),
+            latch_start=nmi_start_latches[first_nmi],
+            num_latches=\
+                nmi_start_latches[last_nmi+1]-nmi_start_latches[first_nmi],
+            apu_accesses=apu_accesses,
+            ex_end_cycle=t_nmis[last_nmi]["end_cycle"],
+            ex_wait_cycle=t_nmis[last_nmi]["wait_cycle"],
+            apu_freqs=[default_real],
+            deviations=[],
+        )
+
+        nmi_groups.append(this_group)
+        these_nmis = []
+
+    return nmi_groups
+nmi_groups = group()
 
 print("chrono figure setup...")
 # connect to and set up chrono figure
@@ -114,7 +163,7 @@ def do_measure():
     # erase save memory to ensure a clean start
     cf.destroy_save_ram()
     # connect to tasha to get the tas started
-    ls.connect(sys.argv[4], status_cb=lambda s: 0,
+    ls.connect(sys.argv[4], status_cb=lambda s: None,
         num_priming_latches=200,
         apu_freq_basic=default_basic,
         apu_freq_advanced=default_advanced,
@@ -131,21 +180,31 @@ def do_measure():
     last_end_cycle = 0
     wrap_cycles = 0
     measurements = []
-    problem_nmis = []
-    while True:
-        ls.communicate() # keep tasha full
+    problem_groups = []
+    events = []
+    finished = False
+    desynced = False
+    for gi, group in enumerate(nmi_groups):
+        group_events = []
+        got_event = False
+        while True:
+            while len(events) == 0 and not finished:
+                ls.communicate() # keep tasha full
+                try:
+                    events.extend(cf.get_events())
+                except KeyboardInterrupt:
+                    raise
+                except:
+                    import traceback
+                    traceback.print_exc()
+                    finished = True
 
-        try:
-            events = cf.get_events()
-        except KeyboardInterrupt:
-            raise
-        except:
-            import traceback
-            traceback.print_exc()
-            break
-        desync = False
-        for event, nmi in zip(events, t_nmis[nmi_num:nmi_num+len(events)]):
-            end_cycle, wait_cycle = event
+                time.sleep(0.1)
+
+            if len(events) == 0: break
+
+            end_cycle, wait_cycle = events[0]
+            events = events[1:]
             if end_cycle < last_end_cycle:
                 wrap_cycles += 2**29
             last_end_cycle = end_cycle
@@ -154,152 +213,116 @@ def do_measure():
             if end_cycle < wait_cycle:
                 wait_cycle -= 2**29
 
-            # expected from the emulator
-            ex_end_cycle, ex_wait_cycle = nmi["end_cycle"], nmi["wait_cycle"]
-            apu_tot = nmi["apu_reads"] + nmi["apu_writes"]
-
-            busy = (F_CYC-end_cycle+wait_cycle)/F_CYC*100
-            ex_busy = (F_CYC-ex_end_cycle+ex_wait_cycle)/F_CYC*100
-
-            measurements.append((end_cycle, wait_cycle))
-
-            do_print = False
-            a_problem = False
-            desync = False
-            if abs(ex_end_cycle-end_cycle) > 1000:
-                print("big nmi diff:", end=" ")
-                do_print = True
-                a_problem = True
-                desync = True
-
-            if (busy > 99.99 and ex_busy <= 99.99) or \
-                    (ex_busy > 99.99 and busy <= 99.99):
-                print("busy desync:", end=" ")
-                do_print = True
-                a_problem = True
-                desync = True
-
-            # if abs(busy-ex_busy) > 1:
-            #     print("big busy diff:", end=" ")
-            #     do_print = True
-
-            if abs(busy-50) > 40 and apu_tot > 4 and busy < 99.999:
-                print("got close:", end=" ")
-                do_print = True
-                a_problem = True
-
-            if abs(ex_busy-50) > 40 and apu_tot > 4 and busy < 99.999:
-                print("should be close:", end=" ")
-                do_print = True
-
-            if a_problem:
-                problem_nmis.append(group_map.get(nmi_num, nmi_num))
-
-            if do_print:
-                print("nmi:{} busy:{:.2f}% ex_busy:{:.2f}% "
-                    "end_cycle:{} ex_end_cycle:{} apu_tot:{}".format(
-                    nmi_num, busy, ex_busy, end_cycle, ex_end_cycle, apu_tot))
-
-            if desync:
-                print("desynchrolized!!")
+            got_event = True
+            group_events.append((end_cycle, wait_cycle))
+            if end_cycle != wait_cycle: # not 100% busy, end of group
                 break
 
-            nmi_num += 1
+        if not got_event: break
 
-        if desync:
+        # store the measurement we made
+        measurements.append(group_events)
+
+        # if this group doesn't have the same number of NMIs, we must have
+        # desynced somehow
+        if len(group_events) != group.num_nmis:
+            print("whoops, desync on group {}".format(gi))
+            problem_groups.append(gi)
+            desynced = True
             break
 
-        time.sleep(0.1)
+        # look at the last nmi in the group
+        end_cycle, wait_cycle = group_events[-1]
+        busy = (F_CYC-end_cycle+wait_cycle)/F_CYC*100
+        closeness = 50-abs(busy-50)
+        if closeness < NMI_CLOSE and group.apu_accesses >= MIN_APU_ACCESSES:
+            print("group {} got close to boundary (within {:.2f}%) and "
+                "accessed the APU {} times".format(
+                    gi, closeness, group.apu_accesses))
+            problem_groups.append(gi)
+
+    if not desynced: # we finished naturally rather than desyncing
+        print("finishing tas...")
+        try:
+            while ls.communicate(): time.sleep(0.1)
+        except KeyboardInterrupt:
+            raise
+        except:
+            pass
 
     print("measurement run complete")
 
-    return measurements, problem_nmis
+    return measurements, problem_groups
 
-acs_template = {
-    "source": "TASHA",
-    "actual": 24.607104,
-    "jitter": 0,
-    "jitter_mode": False,
-    "polarity": False,
-    "applied": 0,
-}
-import pickle
 while True:
-    measurements, problem_nmis = do_measure()
-    pickle.dump((freqs, measurements, problem_nmis), open("bak.pickle", "wb"))
+    print("building TAS")
+    for gi, group in enumerate(nmi_groups):
+        # if there isn't a new frequency, use the last one
+        if len(group.apu_freqs) == len(group.deviations):
+            group.apu_freqs.append(group.apu_freqs[-1])
+        # the last frequency was determined last run as the one to try here
+        basic, advanced, actual = \
+            calculate_advanced(group.apu_freqs[-1])
+        # correct the frequency tried to the one we will actually output
+        group.apu_freqs[-1] = actual
+        # apply the register settings to this group's latches
+        latch_start = group.latch_start
+        latch_end = group.latch_start + group.num_latches
+        all_latches[latch_start:latch_end, (4, 5)] = (basic, advanced)
+
+    measurements, problem_groups = do_measure()
+
     print("updating timeline...")
-
-    rid = len(timeline["runs"])
-    run = {"name": "hello", "apu_clock_source": acs_template.copy()}
-    timeline["runs"].append(run)
-    for mi, m in enumerate(measurements):
-        acs = acs_template.copy()
-        acs["applied"] = t_ls[nmi_start_latches[mi]]
-        acs["actual"] = freqs[nmi_start_latches[mi]]
-        measurement = {
-            "rid": rid,
-            "wait_cycle": m[1],
-            "end_cycle": m[0],
-            "apu_clock_source": acs,
-        }
-        t_nmis[mi]["measurements"].append(measurement)
-
-    json.dump(timeline, open(sys.argv[2], "w"), indent=" ")
+    for group, measurement in zip(nmi_groups, measurements):
+        group.deviations.append(measurement[-1][1]-group.ex_end_cycle)
 
     print("fixing problems")
-    for pi in problem_nmis:
-        print("NMI {}".format(pi))
+    for gi in problem_groups:
+        print("GROUP {}".format(gi))
         print("------------")
-        nmi = t_nmis[pi]
+        
+        group = nmi_groups[gi]
+        measurement = measurements[gi]
 
-        end_cycle, wait_cycle = measurements[pi]
-
-        # expected from the emulator
-        ex_end_cycle, ex_wait_cycle = nmi["end_cycle"], nmi["wait_cycle"]
-        apu_tot = nmi["apu_reads"] + nmi["apu_writes"]
-
+        end_cycle, wait_cycle = measurement[-1]
         busy = (F_CYC-end_cycle+wait_cycle)/F_CYC*100
-        ex_busy = (F_CYC-ex_end_cycle+ex_wait_cycle)/F_CYC*100
+        closeness = 50-abs(busy-50)
 
-        if abs(ex_end_cycle-end_cycle) > 1000:
+        if len(measurement) != group.num_nmis:
             print("problem: desync")
-            if apu_tot < 4:
-                print("but this NMI does not access the APU. can't help ya "
+            if group.apu_accesses < MIN_APU_ACCESSES:
+                print("but this group does not access the APU. can't help ya "
                     "there, bud")
                 continue
         else:
-            print("problem: got within {:.2f}% of a frame boundary "
-                "(less than 10%)".format(50-abs(busy-50)))
+            print("problem: got within {:.2f}% of a frame boundary".format(
+                closeness))
 
-        print("curr APU freq: {:.6f}MHz".format(freqs[nmi_start_latches[pi]]))
+        print("curr APU freq: {:.6f}MHz".format(group.apu_freqs[-1]))
 
+        # calculate expected business if we had a desync as the business above
+        # will be nonsense
+        if len(measurement) != group.num_nmis:
+            busy = (F_CYC-group.ex_end_cycle+group.ex_wait_cycle)/F_CYC*100
         if busy < 50: # bump to 20%
-            target = ex_end_cycle - F_CYC*.8
+            target = group.ex_end_cycle - F_CYC*(1-(NMI_CLOSE_TARGET)/100)
         else: # bump to 80%
-            target = ex_end_cycle - F_CYC*.2
+            target = group.ex_end_cycle - F_CYC*(NMI_CLOSE_TARGET/100)
 
-        # figure out APU frequency vs. cycle deviation (from end of nmi)
-        deviations = []
-        for m in nmi["measurements"]:
-            deviations.append((m["wait_cycle"]-ex_end_cycle,
-                m["apu_clock_source"]["actual"]))
-        print(deviations)
-        deviations = np.asarray(deviations)
-
-        if len(deviations) < 3:
+        if len(group.apu_freqs) < 3:
             print("not enough data to predict, so guessing wildly")
-            new_freq = freqs[nmi_start_latches[pi]]
+            new_freq = group.apu_freqs[-1]
             delta = random.random()/10
             if new_freq > 24.64:
                 new_freq -= delta
             else:
                 new_freq += delta
         else:
-            m, b = np.polyfit(deviations[:, 0], deviations[:, 1], 1)
-            r = np.corrcoef(deviations[:, 0], deviations[:, 1])[0, 1]
+            m, b = np.polyfit(group.deviations, group.apu_freqs, 1)
+            r = np.corrcoef(group.deviations, group.apu_freqs)[0, 1]
             if r != r: # true if r is NaN
                 print("prediction failed, guessing wildly")
-                new_freq = freqs[nmi_start_latches[pi]]
+                new_freq = group.apu_freqs[-1]
                 delta = random.random()/10
                 if new_freq > 24.64:
                     new_freq -= delta
@@ -308,17 +331,11 @@ while True:
             else:
                 print("predicting based on linear fit with "
                     "R^2={:.3f}".format(r**2))
-                new_freq = m*(target-ex_end_cycle) + b
+                new_freq = m*(target-group.ex_end_cycle) + b
                 print("m={}, b={}, new_freq={}".format(m, b, new_freq))
 
         new_freq = min(max(new_freq, 23), 24.75)
-        basic, advanced, actual = calculate_advanced(new_freq, jitter=4)
         print("new APU freq: {:.6f}MHz".format(new_freq))
-
-        for li in range(nmi_start_latches[pi], nmi_start_latches[pi+1]):
-            print("set", li, actual)
-            freqs[li] = actual
-            all_latches[li, 4] = basic
-            all_latches[li, 5] = advanced
+        group.apu_freqs.append(new_freq) # will be tried next time
 
         print()
