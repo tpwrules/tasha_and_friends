203ea73185b645c4b0e54e00134cb5c0ee097aad
Thomas Watson <twatson52@icloud.com>
2020-08-22 14:37:07 -0500

    chrono_figure.gateware: begin eventuator but don't implement anything in it yet

--- chrono_figure/gateware/core.py
+++ chrono_figure/gateware/core.py
@@ -6,10 +6,12 @@ from nmigen.back import verilog
 from .match_info import *
 from .snes_bus import SNESBus
 from .match_engine import MatchEngine, make_match_info
+from .eventuator.eventuator import Eventuator
+from .eventuator import widths
 
 # will probably always be manually incremented because it's related to the
 # modules in the sd2snes and its firmware as well
-GATEWARE_VERSION = 5
+GATEWARE_VERSION = 1000
 
 class ChronoFigureCore(Elaboratable):
     def __init__(self, cart_signals):
@@ -28,6 +30,10 @@ class ChronoFigureCore(Elaboratable):
         self.event_fifo = SyncFIFOBuffered(width=31, depth=128)
         self.bus = SNESBus(cart_signals)
         self.match_engine = MatchEngine()
+        
+        self.eventuator = Eventuator()
+        self.ev_prg_mem = Memory(width=widths.INSN_WIDTH, depth=1024, init=[0])
+        self.ev_reg_mem = Memory(width=widths.DATA_WIDTH, depth=256)
 
     def elaborate(self, platform):
         m = Module()
@@ -36,9 +42,17 @@ class ChronoFigureCore(Elaboratable):
         m.submodules.event_fifo = event_fifo = self.event_fifo
         m.submodules.match_engine = match_engine = self.match_engine
 
-        match_info = make_match_info()
-        match_valid = Signal()
+        m.submodules.eventuator = eventuator = self.eventuator
+        m.submodules.ev_prg_rd = ev_prg_rd = self.ev_prg_mem.read_port(
+            transparent=False)
+        m.submodules.ev_prg_wr = ev_prg_wr = self.ev_prg_mem.write_port()
+        m.submodules.ev_reg_rd = ev_reg_rd = self.ev_reg_mem.read_port(
+            transparent=False)
+        m.submodules.ev_reg_wr = ev_reg_wr = self.ev_reg_mem.write_port()
+
+        # hook up match engine to the bus
         m.d.comb += [
+            # temporary, will be attached to eventuator soon
             match_engine.i_config.eq(self.i_config),
             match_engine.i_config_addr.eq(self.i_config_addr),
             match_engine.i_config_we.eq(self.i_config_we),
@@ -47,98 +61,35 @@ class ChronoFigureCore(Elaboratable):
             match_engine.i_bus_addr.eq(bus.o_addr),
             match_engine.i_bus_data.eq(bus.o_data),
             match_engine.i_cycle_count.eq(bus.o_cycle_count),
-
-            Cat(*match_info).eq(Cat(*match_engine.o_match_info)),
-            match_valid.eq(match_engine.o_match_valid),
-            match_engine.i_match_re.eq(match_engine.o_match_valid),
         ]
 
-        match_cycle = Signal(29)
-        matched_type = Signal(MATCH_TYPE_BITS)
+        # hook up the eventuator's inputs and outputs
         m.d.comb += [
-            match_cycle.eq(match_info.cycle_count),
-            matched_type.eq(Mux(match_valid, match_info.match_type, 0)),
+            Cat(*eventuator.i_match_info).eq(Cat(*match_engine.o_match_info)),
+            eventuator.i_match_valid.eq(match_engine.o_match_valid),
+            match_engine.i_match_re.eq(eventuator.o_match_re),
+
+            event_fifo.w_data.eq(eventuator.o_event),
+            event_fifo.w_en.eq(eventuator.o_event_valid),
+            eventuator.i_event_space.eq(event_fifo.w_rdy),
         ]
 
-        # keep track of when the SNES started waiting for NMI
-        wait_cycle = Signal(29)
-        currently_waiting = Signal()
-        # keep track of which event this is. this is just used for ensuring no
-        # data is lost.
-        event_counter = Signal(2)
+        # hook the eventuator's memories to it
+        m.d.comb += [
+            ev_prg_rd.addr.eq(eventuator.o_prg_addr),
+            ev_prg_rd.en.eq(1),
+            eventuator.i_prg_data.eq(ev_prg_rd.data),
 
-        # data for an event (the NMI). this data is stored in the event FIFO for
-        # the rest of the system
-        event_occurred = Signal()
-        event_data0 = Signal(30)
-        event_data1 = Signal(30)
+            ev_reg_rd.addr.eq(eventuator.o_reg_raddr),
+            ev_reg_rd.en.eq(eventuator.o_reg_re),
+            eventuator.i_reg_rdata.eq(ev_reg_rd.data),
 
-        snes_cycle_counter = Signal(29)
-        counter_offs = Signal(29)
-        m.d.comb += snes_cycle_counter.eq(match_cycle-counter_offs)
+            ev_reg_wr.addr.eq(eventuator.o_reg_waddr),
+            ev_reg_wr.en.eq(eventuator.o_reg_we),
+            ev_reg_wr.data.eq(eventuator.o_reg_wdata),
+        ]
 
-        with m.Switch(matched_type):
-            with m.Case(MATCH_TYPE_RESET):
-                m.d.sync += [
-                    counter_offs.eq(match_cycle),
-                    currently_waiting.eq(0),
-                    # first event after reset is 0, then 1, 2, 3, 1, 2, 3, etc.
-                    event_counter.eq(0),
-                ]
-            with m.Case(MATCH_TYPE_NMI):
-                m.d.comb += event_occurred.eq(1)
-                m.d.sync += [
-                    # first data is the cycle the NMI happened on and the low
-                    # bit of the event counter
-                    event_data0.eq(Cat(
-                        snes_cycle_counter,
-                        event_counter[0],
-                    )),
-                    # second data is the cycle the SNES started waiting for NMI
-                    # and the high bit of the event counter
-                    event_data1.eq(Cat(
-                        # or the current cycle if it never waited
-                        Mux(currently_waiting, wait_cycle, snes_cycle_counter),
-                        event_counter[1],
-                    )),
-                ]
-
-                m.d.sync += [
-                    # don't roll back to 0 so that we can know that 0 is always
-                    # the first event after reset
-                    event_counter.eq(Mux(event_counter==3, 1, event_counter+1)),
-                    currently_waiting.eq(0),
-                ]
-            with m.Case(MATCH_TYPE_WAIT_START):
-                with m.If(~currently_waiting):
-                    m.d.sync += [
-                        wait_cycle.eq(snes_cycle_counter),
-                        currently_waiting.eq(1),
-                    ]
-            with m.Case(MATCH_TYPE_WAIT_END):
-                m.d.sync += currently_waiting.eq(0)
-
-        # write all the pieces of event data to the fifo
-        with m.FSM("IDLE"):
-            with m.State("IDLE"):
-                with m.If(event_occurred):
-                    m.next = "DATA0"
-
-            with m.State("DATA0"):
-                m.d.comb += [
-                    # high bit is 1 for the first word (and 0 for all the rest)
-                    event_fifo.w_data.eq(Cat(event_data0, 1)),
-                    event_fifo.w_en.eq(1),
-                ]
-                m.next = "DATA1"
-
-            with m.State("DATA1"):
-                m.d.comb += [
-                    event_fifo.w_data.eq(Cat(event_data1, 0)),
-                    event_fifo.w_en.eq(1),
-                ]
-                m.next = "IDLE"
-
+        # hook up event fifo to the output
         m.d.comb += [
             self.o_event.eq(event_fifo.r_data),
             self.o_event_valid.eq(event_fifo.r_rdy),
---
+++ chrono_figure/gateware/eventuator/widths.py
@@ -0,0 +1,6 @@
+# info on the widths of various parts of this deal
+
+INSN_WIDTH = 18
+PC_WIDTH = 12
+DATA_WIDTH = 32
+REGS_WIDTH = 8
