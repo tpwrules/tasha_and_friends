d722a578df2a1b1bf8a08b8890105379af2e48b0
Thomas Watson <twatson52@icloud.com>
2020-08-23 20:33:36 -0500

    chrono_figure.gateware.eventuator.sim.test_core: add test of different instructions executing together

--- chrono_figure/gateware/eventuator/sim/test_core.py
+++ chrono_figure/gateware/eventuator/sim/test_core.py
@@ -1,6 +1,6 @@
-# test basic operation of the core to ensure that it generates the correct
-# control signals for all the instructions. does not check the actual data
-# processing of the instructions themselves.
+# test basic logic of the core to ensure that it generates the correct control
+# signals for all the instructions. does not check the actual data processing of
+# the instructions themselves (except what POKE writes).
 
 from nmigen import *
 
@@ -12,7 +12,7 @@ import unittest
 
 class TestCore(SimCoreTest, unittest.TestCase):
     @cycle_test
-    def test_BRANCH(self):
+    def test_BRANCH_logic(self):
         prg = [
             BRANCH(5),
             BRANCH(1),
@@ -39,7 +39,7 @@ class TestCore(SimCoreTest, unittest.TestCase):
         return sets, chks, vals, self.proc_start_prg(prg)
 
     @cycle_test
-    def test_COPY_to_spl(self):
+    def test_COPY_to_spl_logic(self):
         prg = [
             COPY(Special.TEST, 1),
             COPY(Special.TEST, 2),
@@ -74,7 +74,7 @@ class TestCore(SimCoreTest, unittest.TestCase):
         return sets, chks, vals, self.proc_start_prg(prg)
 
     @cycle_test
-    def test_COPY_from_spl(self):
+    def test_COPY_from_spl_logic(self):
         prg = [
             COPY(1, Special.TEST),
             COPY(2, Special.TEST),
@@ -110,7 +110,7 @@ class TestCore(SimCoreTest, unittest.TestCase):
         return sets, chks, vals, self.proc_start_prg(prg)
 
     @cycle_test
-    def test_POKE(self):
+    def test_POKE_logic(self):
         prg = [
             POKE(Special.TEST, 256),
             POKE(Special.TEST, 4),
@@ -138,14 +138,13 @@ class TestCore(SimCoreTest, unittest.TestCase):
         return sets, chks, vals, self.proc_start_prg(prg)
 
     @cycle_test
-    def test_MODIFY(self):
+    def test_MODIFY_logic(self):
         prg = [
             MODIFY(Mod.COPY, 1),
             MODIFY(Mod.COPY, 2),
             MODIFY(Mod.COPY, 3),
         ]
-        sets = {"s": self.core.i_ctl_start,
-                "spc": self.core.i_ctl_pc}
+        sets = {}
         chks = {"rraddr": self.core.o_reg_raddr,
                 "rwaddr": self.core.o_reg_waddr,
                 "rre": self.core.o_reg_re,
@@ -169,6 +168,66 @@ class TestCore(SimCoreTest, unittest.TestCase):
                   "rwaddr": 3, "type": Mod.COPY}),
             # program stopped, no more activity
             ({}, {"rre": 0, "rwe": 0, "mod": 0}),
+        ]
+
+        return sets, chks, vals, self.proc_start_prg(prg)
+
+    @cycle_test
+    def test_mixed_logic(self):
+        prg = [
+            MODIFY(Mod.COPY, 1),
+            COPY(Special.TEST, 2),
+            BRANCH(7),
+            # 4
+            POKE(Special.TEST, 3),
+            COPY(4, Special.TEST),
+            BRANCH(0),
+            # 7
+            POKE(Special.TEST, 5),
+            MODIFY(Mod.COPY, 6),
+            BRANCH(4),
+        ]
+        sets = {}
+        chks = {"rraddr": self.core.o_reg_raddr,
+                "rwaddr": self.core.o_reg_waddr,
+                "rre": self.core.o_reg_re,
+                "rwe": self.core.o_reg_we,
+                "saddr": self.core.o_spl_addr,
+                "sdata": self.core.o_spl_data,
+                "sre": self.core.o_spl_re,
+                "swe": self.core.o_spl_we,
+                "mod": self.core.o_mod,
+                "type": self.core.o_mod_type}
+        vals = [
+            # program is starting
+            ({}, {"rre": 0, "rwe": 0, "sre": 0, "swe": 0, "mod": 0}),
+            # fetch: MODIFY
+            ({}, {"rre": 1, "rwe": 0, "sre": 0, "swe": 0, "mod": 0,
+                  "rraddr": 1}),
+            # fetch: COPY, exec: MODIFY
+            ({}, {"rre": 1, "rwe": 1, "sre": 0, "swe": 0, "mod": 1,
+                  "rraddr": 2, "rwaddr": 1, "type": Mod.COPY}),
+            # fetch: BRANCH, exec: COPY
+            ({}, {"rre": 0, "rwe": 0, "sre": 0, "swe": 1, "mod": 0,
+                  "saddr": Special.TEST}),
+            # fetch: POKE, exec: BRANCH
+            ({}, {"rre": 0, "rwe": 0, "sre": 0, "swe": 0, "mod": 0}),
+            # fetch: MODIFY, exec: POKE
+            ({}, {"rre": 1, "rwe": 0, "sre": 0, "swe": 1, "mod": 0,
+                  "rraddr": 6, "saddr": Special.TEST, "sdata": 5}),
+            # fetch: BRANCH, exec: MODIFY
+            ({}, {"rre": 0, "rwe": 1, "sre": 0, "swe": 0, "mod": 1,
+                  "rwaddr": 6, "type": Mod.COPY}),
+            # fetch: POKE, exec: BRANCH
+            ({}, {"rre": 0, "rwe": 0, "sre": 0, "swe": 0, "mod": 0}),
+            # fetch: COPY, exec: POKE
+            ({}, {"rre": 0, "rwe": 0, "sre": 1, "swe": 1, "mod": 0,
+                  "saddr": Special.TEST, "sdata": 3}),
+            # fetch: BRANCH, exec: COPY
+            ({}, {"rre": 0, "rwe": 1, "sre": 0, "swe": 0, "mod": 0,
+                  "rwaddr": 4}),
+            # program stopped, no more activity
+            ({}, {"rre": 0, "rwe": 0, "sre": 0, "swe": 0, "mod": 0}),
         ]
 
         return sets, chks, vals, self.proc_start_prg(prg)
